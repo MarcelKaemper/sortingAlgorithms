7f95bc6ab802100349704a658695dc914f04e708
Marcel <marcelkaemper862@gmail.com>
2019-01-10 18:59:35 +0100

    Implemented bubbleSort in python

--- python/main.py
+++ python/main.py
@@ -1,19 +1,37 @@
 class Sorter:
-	def __init__(self, list, sortedList, indexSmallest):
-		self.unsortedList = list
-		self.sortedList = []
-		self.indexSmallest = 0
+	def __init__(self, list):
+		self.list = list
 
 	def selectionSort(self):
-		print(self.unsortedList)
-		while len(self.unsortedList)>0:
-			for i in range(len(self.unsortedList)):
-				if self.unsortedList[i] <= self.unsortedList[self.indexSmallest]:
-					self.indexSmallest = i
-			self.sortedList.append(self.unsortedList[self.indexSmallest])
-			self.unsortedList.pop(self.indexSmallest)
-			self.indexSmallest = 0
-		return self.sortedList	
+		unsortedList = self.list[:]
+		sortedList = []
+		indexSmallest = 0
 
-sorter = Sorter([2,6,1,8,5], [], 0)
+		while len(unsortedList)>0:
+			for i in range(len(unsortedList)):
+				if unsortedList[i] <= unsortedList[indexSmallest]:
+					indexSmallest = i
+			sortedList.append(unsortedList[indexSmallest])
+			unsortedList.pop(indexSmallest)
+			indexSmallest = 0
+		return sortedList
+
+	def bubbleSort(self):
+		list = self.list[:]
+		print("List:",list)
+		moves = 0
+		notDone = True
+		while notDone:
+			notDone = False
+			for i in range(len(list)):
+				if(i != len(list)-1):
+					if(list[i]>list[i+1]):
+						temp = list[i]
+						list[i] = list[i+1]
+						list[i+1] = temp
+						notDone = True
+		return list
+
+sorter = Sorter([2,6,1,8,5])
 print(sorter.selectionSort())
+print(sorter.bubbleSort())
